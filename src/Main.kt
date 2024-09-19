fd315bfb64303a8f5a49ca1b6d787ef904ed298f
ribalinux <esimarsa@gmail.com>
2024-09-19 16:55:32 +0100

    19/09/2024

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
     val cpu = (1..10).random()
+
+    println("Por favor escolha um numero entre 1 e 10:")
+    val player = readln().toIntOrNull()
+
+    if (player in 1..10) {
+        if (player == cpu) {
+            println("O CPU escolheu : $cpu")
+            println("Parabens acertou no numero")
+        } else {
+            println("O CPU escolheu : $cpu")
+            println("Perdeu ;(")
+        }
+    } else {
+        println("Input Invalido")
+    }
 }
